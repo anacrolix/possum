75bc2195864e67f5905f689da854ad88c6591cd3
Matt Joiner <anacrolix@gmail.com>
2023-12-13 23:18:08 +1100

    Add more C API

--- possum.h
+++ possum.h
@@ -3,6 +3,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct BatchWriter BatchWriter;
+
 typedef struct Handle Handle;
 
 struct Handle *possum_new(const char *path);
+
+void possum_drop(struct Handle *handle);
+
+size_t possum_single_write_buf(struct Handle *handle,
+                               const unsigned char *key,
+                               size_t key_size,
+                               const uint8_t *value,
+                               size_t value_size);
+
+struct BatchWriter *possum_new_writer(struct Handle *handle);
--- src/c_api.rs
+++ src/c_api.rs
@@ -1,7 +1,9 @@
 use super::*;
+use libc::size_t;
 use log::error;
-use std::ffi::{c_char, CStr, OsStr};
+use std::ffi::{c_char, c_uchar, CStr, OsStr};
 use std::ptr::null_mut;
+use std::slice;
 
 #[no_mangle]
 pub extern "C" fn possum_new(path: *const c_char) -> *mut Handle {
@@ -15,4 +17,42 @@ pub extern "C" fn possum_new(path: *const c_char) -> *mut Handle {
         }
     };
     Box::into_raw(Box::new(handle))
+}
+
+// This drops the Handle Box. Instead, if this is hard to use correctly from C, it could drop a top-level reference
+// count for the box. i.e. If this one goes, there's no way to work with the Handle, and when all other outstanding
+// operations on the Handle complete, it will drop the Handle for real.
+#[no_mangle]
+pub extern "C" fn possum_drop(handle: *mut Handle) {
+    drop(unsafe { Box::from_raw(handle) })
+}
+
+#[no_mangle]
+pub extern "C" fn possum_single_write_buf(
+    handle: *mut Handle,
+    key: *const c_uchar,
+    key_size: size_t,
+    value: *const u8,
+    value_size: size_t,
+) -> size_t {
+    let key_vec = unsafe { slice::from_raw_parts(key, key_size) }.to_vec();
+    let value_slice = unsafe { slice::from_raw_parts(value, value_size) };
+    const ERR_SENTINEL: usize = usize::MAX;
+    let handle = unsafe { &*handle };
+    match handle.single_write_from(key_vec, value_slice) {
+        Err(_) => ERR_SENTINEL,
+        Ok((n, _)) => {
+            let n = n.try_into().unwrap();
+            assert_ne!(n, ERR_SENTINEL);
+            n
+        }
+    }
+    .try_into()
+    .unwrap()
+}
+
+#[no_mangle]
+pub extern "C" fn possum_new_writer(handle: *mut Handle) -> *mut BatchWriter<'static> {
+    let handle = unsafe { &*handle };
+    Box::into_raw(Box::new(handle.new_writer().unwrap()))
 }

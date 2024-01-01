ccf18106ebbd5940b1360161b0c1c8418b7853d7
Matt Joiner <anacrolix@gmail.com>
2024-01-01 17:46:08 +1100

    Change file_id manifest type from any to blob

--- manifest.sql
+++ manifest.sql
@@ -2,7 +2,10 @@
 
 create table if not exists keys (
     key_id integer primary key,
-    file_id any,
+    -- This is to support whatever the OS can use for paths. It was any for a while to support
+    -- migrating to different value file naming schemes, but since this is intended for caches,
+    -- maybe it's not worth the risk.
+    file_id blob,
     file_offset integer,
     value_length integer,
     -- This is the most (concrete?) representation for the finest time granularity sqlite's internal time functions support.

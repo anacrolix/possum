a11cd19e3556aec3b970c429328b9c38fbef06b8
Matt Joiner <anacrolix@gmail.com>
2024-01-11 10:29:09 +1100

    Fix clonefile benchmarks to create file only when run

--- benches/clonefile.rs
+++ benches/clonefile.rs
@@ -5,24 +5,28 @@ use possum::clonefile::clonefile;
 use possum::testing::test_tempdir;
 
 fn clonefile_benchmark_fallible(c: &mut Criterion) -> anyhow::Result<()> {
-    let tempdir = test_tempdir("benchmark_clonefile")?;
+    let mut group = c.benchmark_group("clonefile");
     for size_power in [12, 20, 28] {
         let len = 1 << size_power;
-        let file = possum::testing::write_random_tempfile(len)?;
-        let dst_path = tempdir.path.join("hello");
-        let mut group = c.benchmark_group("clonefile");
         group.bench_with_input(
             BenchmarkId::new("hello", bytesize::ByteSize(len).to_string_as(true)),
-            &file,
-            |b, file| {
-                b.iter(|| {
-                    (|| -> anyhow::Result<()> {
-                        let _ = remove_file(&dst_path);
-                        clonefile(file.path(), dst_path.as_ref())?;
-                        Ok(())
-                    })()
-                    .unwrap()
-                })
+            &len,
+            |b, file_size| {
+                (|| -> anyhow::Result<()> {
+                    let tempdir = test_tempdir("benchmark_clonefile")?;
+                    let file = possum::testing::write_random_tempfile(*file_size).unwrap();
+                    let dst_path = tempdir.path.join("hello");
+                    b.iter(|| {
+                        (|| -> anyhow::Result<()> {
+                            let _ = remove_file(&dst_path);
+                            clonefile(file.path(), dst_path.as_ref())?;
+                            Ok(())
+                        })()
+                        .unwrap()
+                    });
+                    Ok(())
+                })()
+                .unwrap()
             },
         );
     }

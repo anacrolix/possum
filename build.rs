0879105ae7963425b7a3846547b448ae674d0e52
Matt Joiner <anacrolix@gmail.com>
2025-05-28 14:53:08 +1000

    Generate possum.h as part of build
    Needed to ensure C++ bindings aren't produced accidentally.

--- build.rs
+++ build.rs
@@ -1,4 +1,14 @@
+use std::env;
+
 fn main() {
     // We have a custom "loom" cfg that Rust warns about since 1.80.
     println!("cargo::rustc-check-cfg=cfg(loom)");
+
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::generate(crate_dir)
+        .expect("Unable to generate bindings")
+        // I expect there's a better place to put this, then copy it into the Go
+        // directory so it is packaged in the Go module.
+        .write_to_file("go/cpossum/possum.h");
 }

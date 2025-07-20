6a3e4f7e8da33c9f9e6fa1fb7815afca8128f69f
Matt Joiner <anacrolix@gmail.com>
2025-07-20 12:51:41 +1000

    Claude said to add some library links for Windows

--- build.rs
+++ build.rs
@@ -4,6 +4,18 @@ fn main() {
     // We have a custom "loom" cfg that Rust warns about since 1.80.
     println!("cargo::rustc-check-cfg=cfg(loom)");
 
+    // Link Windows system libraries when building for Windows
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=kernel32");
+        println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=bcrypt");
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=oleaut32");
+        println!("cargo:rustc-link-lib=userenv");
+        println!("cargo:rustc-link-lib=ntdll");
+        println!("cargo:rustc-link-lib=synchronization");
+    }
+
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::generate(crate_dir)
